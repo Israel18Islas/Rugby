4b42f3f12d30a3e0c16e5c03434ed5b00303ea1b
i7 <i7@Lenovo>
2017-10-04 11:10:24 -0700

    Actualizacion: Cancha Class con dos atributos con sus setters y getters

---
+++ src/Cancha.java
@@ -0,0 +1,22 @@
+public class Cancha {
+
+	public boolean Porterias;
+	public String Balon;
+
+	public boolean isPorterias() {
+		return this.Porterias;
+	}
+
+	public void setPorterias(boolean Porterias) {
+		this.Porterias = Porterias;
+	}
+
+	public String getBalon() {
+		return this.Balon;
+	}
+
+	public void setBalon(String Balon) {
+		this.Balon = Balon;
+	}
+
+}
