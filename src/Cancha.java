a512c96f0a2a3de081b8c8bee0e01b9158d577e9
i7 <i7@Lenovo>
2017-10-04 19:47:55 -0700

    Actualizacion: Coordenadas de la cancha

--- src/Cancha.java
+++ src/Cancha.java
@@ -1,13 +1,21 @@
 public class Cancha {
 
-	public boolean Porterias;
+	public String Porterias;
 	public String Balon;
+	/**
+	 * Coordenada X
+	 */
+	public int x = 0;
+	/**
+	 * Coordenada Y
+	 */
+	public int y = 0;
 
-	public boolean isPorterias() {
+	public String getPorterias() {
 		return this.Porterias;
 	}
 
-	public void setPorterias(boolean Porterias) {
+	public void setPorterias(String Porterias) {
 		this.Porterias = Porterias;
 	}
 
@@ -17,6 +25,22 @@ public class Cancha {
 
 	public void setBalon(String Balon) {
 		this.Balon = Balon;
+	}
+
+	public int getX() {
+		return this.x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return this.y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
 	}
 
 }
